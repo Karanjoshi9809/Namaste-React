7e6745aebf3fd4fea408f3d65b2b8dd8ffab4908
Karan Joshi <karanjoshi9809@gmail.com>
2024-02-24 10:14:03 +0530

    Ignite app : Learned about bundlers

--- App.js
+++ App.js
@@ -4,6 +4,9 @@
 // Creating single html element
 // const heading = React.createElement("h1", {}, "Welcome to Namaste React") 
 
+import React from "react"
+import ReactDOM from "react-dom/client"
+
 // Creating multiple elements
 const heading1 = React.createElement("h1", {}, "Welcome to Namaste React")
 const heading2 = React.createElement("h2", {}, "This is super cool & knowledge heavy react course")
@@ -21,3 +24,27 @@ const contaier = React.createElement(
 const root = ReactDOM.createRoot(document.getElementById('root'))
 
 root.render(contaier)
+
+/*
+    Parcel or any bundler is beast
+    They do following things
+        Created A Server
+        * HMR - Hot Module Replacement
+        * File Watcher algorithm - C++
+        * BUNDLING
+        * MINIFY
+        * Cleaning our Code
+        * Dev abd Production Build
+        * Super Fast build algorithm
+        * Image Optimization
+        * Caching while development
+        * Compression
+        * Compatble with older version of browser
+        * HTTPS on dev
+        * port Number
+        * Consistent Hashing Algorithm
+        * Zero Config
+        * 
+        * 
+        * Transitive Dependencies 
+*/
